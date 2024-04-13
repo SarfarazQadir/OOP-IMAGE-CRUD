cdfc02f02a9f1fbc62e457a796ff60436f0075cb
SarfarazQadir <sarfarazqadir594@gmail.com>
2024-04-13 22:29:32 +0500

    2nd commit

--- fetch.php
+++ fetch.php
@@ -6,7 +6,7 @@ include ("config.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Fetch Data</title>
 </head>
 <body>
     <table border="2">
